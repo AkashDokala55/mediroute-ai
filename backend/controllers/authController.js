756ab84006f0c62064cdf295d668ba9861463398
AkashDokala55 <akashdokala7@gmail.com>
2026-06-11 20:13:49 +0530

    Log email user and password existence
    Add logging for email user and password existence

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -428,7 +428,8 @@ const transporter =
 ) => {
 
   try {
-
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
     const { email } =
       req.body;
 
