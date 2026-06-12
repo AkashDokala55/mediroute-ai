7f1675cf944a686cc6f3402abca7b972205d9186
AkashDokala55 <akashdokala7@gmail.com>
2026-06-12 10:01:47 +0530

    Update email transport configuration to use Brevo

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -188,22 +188,14 @@ const registerUser = async (
 };
 const transporter =
   nodemailer.createTransport({
-    host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
 
     auth: {
-      user:
-        process.env.EMAIL_USER,
-
-      pass:
-        process.env.EMAIL_PASS,
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
     },
-    connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-
     tls: {
       rejectUnauthorized:
         false,
